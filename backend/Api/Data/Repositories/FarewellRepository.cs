3c33f13d6f7825aa9983382a916b855aca587338
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2024-08-24 15:09:59 -0400

    Add farewell endpoint

---
+++ backend/Api/Data/Repositories/FarewellRepository.cs
@@ -0,0 +1,28 @@
+﻿namespace Api.Data.Repositories;
+
+public class FarewellRepository
+(
+    DatabaseContext databaseContext
+)
+{
+    private string _farewell = string.Empty;
+
+    private const string GetFirstFarewellRecord = "SELECT farewell FROM farewells LIMIT 1;";
+
+    public string GetFarewell()
+    {
+        if (_farewell != string.Empty) return _farewell;
+
+        using var connection = databaseContext.CreateConnection();
+
+        using var cmd = new MySqlCommand(GetFirstFarewellRecord, connection);
+
+        using var reader = cmd.ExecuteReader();
+
+        reader.Read();
+
+        _farewell = reader.GetString(0);
+
+        return _farewell;
+    }
+}
